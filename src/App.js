90d90b719ef2f11b527a61ac35f8c5909b0e771d
praispranav <praispranav@gmail.com>
2021-04-21 10:03:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Components from "./Components"
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello this is Pranav NAd who are you.</h1>
+    <Components />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class Components extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Gmail   </h1>
+            </div>
+        )
+    }
+}
+
+export default Components
